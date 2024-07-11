b8f34cd16e58d76e6d643a0371aa3cb88ce8a880
idah-github <guantaiidah@gmail.com>
2024-07-11 09:28:47 +0000

    min-ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+In a text file, there is a single character H. Your text editor can execute
+    only two operations in this file: Copy All and Paste. Given a number n,
+    write a method that calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
+    """
+    Function calculating minoperations recursively
+    """
+    if n <= 1:
+        return 0
+    for i in range(2, n + 1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+    return 0
